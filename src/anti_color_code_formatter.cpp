950bc3b411020edd181fdf6277dfce03377224e2
RealBytes86 <echtedeveloper@gmail.com>
2025-04-28 00:56:18 +0200

    todo

--- src/anti_color_code_formatter.cpp
+++ src/anti_color_code_formatter.cpp
@@ -4,21 +4,9 @@
 
 namespace torchcs
 {
+    //TODO: anti_color_code fixen
     void AntiColorCodeFormatter::format(const spdlog::details::log_msg &msg, const std::tm & /*tm*/, spdlog::memory_buf_t &dest) {
         std::string_view log_message(msg.payload.data(), msg.payload.size());
-
-        size_t pos = 0;
-        while ((pos = log_message.find("§", pos)) != std::string_view::npos) {
-            size_t next_pos = log_message.find("§r", pos);
-            if (next_pos != std::string_view::npos) {
-                log_message.remove_prefix(next_pos + 2); 
-            }
-            else {
-                break;
-            }
-        }
-
-        fmt::format_to(std::back_inserter(dest), "{}", log_message);
     }
 
     std::unique_ptr<spdlog::custom_flag_formatter> AntiColorCodeFormatter::clone() const
